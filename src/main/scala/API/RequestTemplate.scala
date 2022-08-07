2ff0e2557f89b7407eb291281ec17d9dc876e7a1
bazylkupka <b.kupka@gamedia.pl>
2022-08-07 20:16:54 +0200

    Add request template for mocking

--- src/main/scala/API/RequestTemplate.scala
+++ src/main/scala/API/RequestTemplate.scala
@@ -1,10 +1,15 @@
 package API
 
+import API.RequestTemplate.TIMEOUT_MILLS
 import requests.Response
 
 class RequestTemplate {
 
   def getRequest(url: String): Response =
-    requests.get(url)
+    requests.get(url, connectTimeout = TIMEOUT_MILLS)
 
+}
+
+object RequestTemplate {
+  val TIMEOUT_MILLS = 1000
 }
