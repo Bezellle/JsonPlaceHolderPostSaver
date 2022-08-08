864194c2e286901b38a080644a795c0b4457cd8c
bazylkupka <b.kupka@gamedia.pl>
2022-08-08 20:36:56 +0200

    Add directory parsing

--- src/main/scala/FlieSaver/PostFileSaver.scala
+++ src/main/scala/FlieSaver/PostFileSaver.scala
@@ -3,17 +3,21 @@ package FlieSaver
 import Domain.{Post, PostId}
 import org.json4s.jackson.Serialization.write
 import org.json4s.{DefaultFormats, Formats}
+import validator.DirectoryValidator
 
 
-class PostFileSaver(fileHandler: FileHandler) {
+class PostFileSaver(fileHandler: FileHandler, validator: DirectoryValidator) {
 
   private implicit val formats: Formats = DefaultFormats
 
   def savePost(post: Post, directory: Option[String]): PostId = {
-      fileHandler.saveFile(composeFileName(post), write(post))
-      post.id
-    }
+    fileHandler.saveFile(composeFileName(post, directory), write(post))
+    post.id
+  }
 
-  private def composeFileName(post: Post) =
-    s"${post.id.id}.json"
+  private def composeFileName(post: Post, directory: Option[String]): String =
+    directory
+      .filter(dir => validator.isValidDirectory(dir))
+      .map(dir => s"$dir/${post.id.id}.json")
+      .getOrElse(s"${post.id.id}.json")
 }
--- src/main/scala/validator/DirectoryValidator.scala
+++ src/main/scala/validator/DirectoryValidator.scala
@@ -5,7 +5,9 @@ import java.io.File
 class DirectoryValidator {
 
   def isValidDirectory(directoryPath: String): Boolean = {
-    val directory = new File(directoryPath)
-    directory.isDirectory
-  }
+        val directory = new File(directoryPath)
+        directory.isDirectory
+    }
+
+
 }
