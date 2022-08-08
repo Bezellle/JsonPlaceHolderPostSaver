61850396580c2791cf96e9ee637f934c2fa28c7e
bazylkupka <b.kupka@gamedia.pl>
2022-08-08 09:05:29 +0200

    Add postId to saving response

--- src/main/scala/FlieSaver/PostFileSaver.scala
+++ src/main/scala/FlieSaver/PostFileSaver.scala
@@ -1,16 +1,18 @@
 package FlieSaver
 
-import Domain.Post
-import org.json4s.{DefaultFormats, Formats}
+import Domain.{Post, PostId}
 import org.json4s.jackson.Serialization.write
+import org.json4s.{DefaultFormats, Formats}
 
 
 class PostFileSaver(fileHandler: FileHandler) {
 
   private implicit val formats: Formats = DefaultFormats
 
-  def savePost(post: Post): Unit =
-    fileHandler.saveFile(composeFileName(post), write(post))
+  def savePost(post: Post): PostId = {
+      fileHandler.saveFile(composeFileName(post), write(post))
+      post.id
+    }
 
   private def composeFileName(post: Post) =
     s"${post.id.id}.json"
