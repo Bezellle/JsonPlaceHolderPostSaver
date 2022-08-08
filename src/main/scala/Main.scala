518f80afb3325eb2b6e088f14f81e73adaae4faf
bazylkupka <b.kupka@gamedia.pl>
2022-08-08 09:07:43 +0200

    Add service and update main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,24 @@
 import API.{JsonPlaceholderClient, RequestTemplate}
-import Domain.Post
+import FlieSaver.{FileHandler, PostFileSaver}
+import service.PostSavingService
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.util.{Failure, Success, Try}
 
 object Main {
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
     val client = new JsonPlaceholderClient(new RequestTemplate)
-    println(client.getAllPosts)
+    val fileSaver = new PostFileSaver(new FileHandler)
+    val service = new PostSavingService(client, fileSaver)
+
+    val savedPostsIds = service.fetchAndSavePosts()
+
+    Try { Await.result(savedPostsIds, Duration(5, SECONDS)) } match {
+      case Success(result) => println(s"Successfully fetched and saved ${result.size} posts")
+      case Failure(exception) => throw exception
+    }
   }
 }
